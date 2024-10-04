78e2ab4573332cff1305ab65f2621fc88bef4445
Scaastillo97 <sebas.s416721@cesurformacion.com>
2024-10-04 12:08:21 +0200

    Add files via upload

---
+++ TiempoSegundos.java
@@ -0,0 +1,8 @@
+public class TiempoSegundos {
+    public static void main(String[] args) {
+        int segundos = 103;
+        System.out.println(segundos/60 + " minutos " + segundos%60 + " segundos");
+        
+
+    }
+}
